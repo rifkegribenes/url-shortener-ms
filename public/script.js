699a7c571901f28ec5fd6d83f8109d5e683aeef6
Sarah Schneider <rifkegribenes@gmail.com>
2018-01-18 12:42:07 -0800

    added input & button

--- public/script.js
+++ public/script.js
@@ -1,18 +1,11 @@
 // client-side js
 
 document.addEventListener("DOMContentLoaded", () => {
-  const Root_Url = window.location.href;
-	const naturalLink = `<div class="card__url">${Root_Url}December 15, 2015</div><a target="_blank" href="${Root_Url}December 15, 2015" class="card__action">Try Natural Date</a>`;
-	const unixLink = `<div class="card__url">${Root_Url}1450137600</div><a target="_blank" href="${Root_Url}1450137600" class="card__action">Try Unix Timestamp</a>`;
-  const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-  const now = new Date();
-  const dd = now.getDate();
-  const month = months[now.getMonth()];
-  const yyyy = now.getFullYear();
-  const formattedToday = `${month} ${dd}, ${yyyy}`;
-  const today = `<div class="card__url">${Root_Url}${formattedToday}</div><a target="_blank" href="${Root_Url}${formattedToday}}" class="card__action">Try Today's Date</a>`;
-	document.getElementById("natural-link").innerHTML = naturalLink;
-	document.getElementById("unix-link").innerHTML = unixLink;
-  document.getElementById("today").innerHTML = today;
+  const setLink = () => {
+    const Root_Url = window.location.href;
+    const urlToShorten = document.getElementById('urlInput').value;
+    const shortenerLink = `${Root_Url}${urlToShorten}`;
+    document.getElementById("shortenerLink").href = shortenerLink;
+  };
+  document.getElementById('urlInput').addEventListener("keyup", setLink);
 });
-
--- server.js
+++ server.js
@@ -2,18 +2,37 @@
 
 // init project
 const express = require('express');
-const http = require('http');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const api = require('./routes/api');
+app.use(bodyParser.json());
+app.use(cors());
+
+// create DB entry
+app.get('/new/:urlToShorten(*)', (req, res, next) => {
+	const { urlToShorten } = req.params;
+	console.log(urlToShorten);
+});
+
+//
+
+// const api = require('./routes/api');
 
 app.use(express.static('public'));
-app.use('/', api);
+// app.use('/', api);
 
 app.set('view engine', 'pug');
 
+app.get('*', (req, res) => {
+  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  res.render('index.pug', {
+    fullUrl: fullUrl,
+    title: 'URL Shortener Microservice'
+  });
+});
 
-const server = http.createServer(app);
-const port = process.env.PORT || '3000';
-app.set('port', port);
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server listening on localhost:${port}`));
