ce4129bffdf643c5325fc14e2a1e6b8d978d4bc9
Sarah Schneider <rifkegribenes@gmail.com>
2018-01-18 18:07:20 -0800

    add link copy functionality

--- public/script.js
+++ public/script.js
@@ -1,11 +1,49 @@
 // client-side js
 
 document.addEventListener("DOMContentLoaded", () => {
-  const setLink = () => {
-    const Root_Url = window.location.href;
-    const urlToShorten = document.getElementById('urlInput').value;
-    const shortenerLink = `${Root_Url}new/${urlToShorten}`;
-    document.getElementById("shortenerLink").href = shortenerLink;
+  // get refs to elements
+  const urlInput = document.getElementById('urlInput');
+  const shortenButton = document.getElementById('shortenButton');
+  const shorten_actions = document.getElementById('shorten_actions');
+  let shortenerLink;
+
+  const copy = () => {
+    document.execCommand('copy');
   };
-  document.getElementById('urlInput').addEventListener("keyup", setLink);
+
+  const clearInput = () => {
+    urlInput.value = '';
+    urlInput.classList.remove('selected');
+    shorten_actions.classList.remove('visible');
+  }
+
+  const getShortenedLink = () => {
+    // get value of original link to call API
+    const Root_Url = window.location.origin;
+    const urlToShorten = document.getElementById('urlInput').value;
+    apiLink = `${Root_Url}/new/${urlToShorten}`;
+
+    // call API to get shortened link
+    fetch(apiLink)
+      .then(res => res.json())
+      .then((data) => {
+        const shortenedLink = `${Root_Url}${data.shorterUrl}`;
+        urlInput.value = shortenedLink;
+        urlInput.focus();
+        urlInput.setSelectionRange(0, urlInput.value.length);
+        urlInput.classList.add('selected');
+        shorten_actions.classList.add('visible');
+        const close = document.getElementById('clear_active_shorten');
+        const copy = document.getElementById('copy_shortlink');
+        close.addEventListener("click", clearInput);
+        copy.addEventListener("click", copy);
+        }
+      )
+      .catch((err) => {
+          console.log(err);
+      });
+
+  }
+
+  shortenButton.addEventListener("click", getShortenedLink);
 });
